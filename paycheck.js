71b037d2048c6e5124cec2b4d943a1659e81c1e7
Dillon Christensen <j.dillon.christensen@gmail.com>
2020-07-23 20:53:38 -0500

    made the quarantine change. didn't test lol

--- paycheck.js
+++ paycheck.js
@@ -1,54 +1,72 @@
-const async = require('async');
-const db = require('@dillonchr/ephemeraldb');
-const budget = require('./budget.js');
+const async = require("async");
+const db = require("@dillonchr/ephemeraldb");
+const budget = require("./budget.js");
 
 const COLLECTION_NAME = process.env.BANKRUPT_PAYCHECK_COLLECTION;
 
-const extractRemainingBalanceFromPaycheck = (paycheck) => {
-    return paycheck.transactions.reduce((sum, deduction) => sum - deduction, paycheck.balance - process.env.BANKRUPT_BUDGET_CUT);
-};
+function extractRemainingBalanceFromPaycheck(paycheck) {
+  let diff = paycheck.balance - process.env.BANKRUPT_BUDGET_CUT;
+  for (const amt of paycheck.transactions) {
+    diff -= amt;
+  }
+  return diff;
+}
+
+function getBalance(onResponse) {
+  db.findItemInCollection(COLLECTION_NAME, {}, (err, paycheck) => {
+    if (err) {
+      onResponse(err);
+    } else {
+      onResponse(null, extractRemainingBalanceFromPaycheck(paycheck));
+    }
+  });
+}
 
 module.exports = {
-    balance(onResponse) {
-        db.findItemInCollection(COLLECTION_NAME, {}, (err, paycheck) => {
-            if (err) {
-                onResponse(err);
-            } else {
-                onResponse(null, extractRemainingBalanceFromPaycheck(paycheck));
-            }
+  balance(onResponse) {
+    getBalance(onResponse);
+  },
+  spend(amount, onResponse) {
+    db.findItemInCollection(COLLECTION_NAME, {}, (err, paycheck) => {
+      if (err) {
+        onResponse(err);
+      } else {
+        paycheck.transactions.push(amount);
+        db.replaceDocument(COLLECTION_NAME, {}, paycheck, err => {
+          if (err) {
+            onResponse(err);
+          } else {
+            onResponse(null, extractRemainingBalanceFromPaycheck(paycheck));
+          }
         });
-    },
-    spend(amount, onResponse) {
-        db.findItemInCollection(COLLECTION_NAME, {}, (err, paycheck) => {
-            if (err) {
-                onResponse(err);
-            } else {
-                paycheck.transactions.push(amount);
-                db.replaceDocument(COLLECTION_NAME, {}, paycheck, err => {
-                    if (err) {
-                        onResponse(err);
-                    } else {
-                        onResponse(null, extractRemainingBalanceFromPaycheck(paycheck));
-                    }
-                });
-            }
-        });
-    },
-    reset(amount = process.env.BANKRUPT_DEFAULT_PAYCHECK_AMOUNT, onResponse) {
-        const paycheck = {
-            balance: amount,
-            transactions: []
-        };
+      }
+    });
+  },
+  reset(amount = process.env.BANKRUPT_DEFAULT_PAYCHECK_AMOUNT, onResponse) {
+    const paycheck = {
+      balance: amount,
+      transactions: []
+    };
 
-        async.series([
+    getBalance(function (err, balance) {
+      if (err) {
+        onResponse(err);
+      } else {
+        const splitBalance = balance > 0 ? balance * 0.1 : 0;
+        async.series(
+          [
             fn => db.replaceDocument(COLLECTION_NAME, {}, paycheck, fn),
-            fn => budget.reset(fn)
-        ], err => {
+            fn => budget.reset(splitBalance, fn)
+          ],
+          err => {
             if (err) {
-                onResponse(err);
+              onResponse(err);
             } else {
-                onResponse(null, extractRemainingBalanceFromPaycheck(paycheck));
+              onResponse(null, extractRemainingBalanceFromPaycheck(paycheck));
             }
-        });
-    }
+          }
+        );
+      }
+    });
+  }
 };
