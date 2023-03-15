1cfd217451e8a1df58142fa67c5fd88c0d7727dc
Dillon Christensen <j.dillon.christensen@gmail.com>
2023-03-14 23:14:47 -0500

    super simplified, now we don't have any waiting for results or math except during changes

--- bankrupt.js
+++ bankrupt.js
@@ -1,3 +1,49 @@
-const budget = require("./budget.js");
-const paycheck = require("./paycheck.js");
-module.exports = { budget, paycheck };
+const { read, save } = require("./persist.js");
+const { BANKRUPT_DEFAULT_PAYCHECK_AMOUNT: DefaultPaycheckAmt } = process.env;
+
+// this is the in-memory paycheck and budget information
+let register = {};
+
+async function onInit() {
+  register = await read();
+}
+
+function balance(id) {
+  if (null != id && !(id in register)) {
+    register[id] = 0.0;
+  }
+  return register[id];
+}
+
+
+function spend(id, amount) {
+  if (null != id && id in register) {
+    register[id] = register[id] - amount;
+    save(register);
+    return register[id];
+  }
+}
+
+function reset(id, amount = DefaultPaycheckAmt) {
+  const linkedBudgetConfig = register[`linked-budget-${id}`];
+  if (null != linkedBudgetConfig) {
+    const { ids, cut } = linkedBudgetConfig;
+    const prevBalance = register[id];
+    const splitBalance = prevBalance > 0 ? prevBalance * 0.1 : 0;
+    const beginningBal = splitBalance + (cut / ids.length);
+    for (const budgetId of ids) {
+      reset(budgetId, Math.max(register[budgetId], 0.0) + beginningBal);
+    }
+    amount -= cut;
+  }
+  register[id] = amount;
+  save(register);
+  return balance(id);
+}
+
+module.exports = {
+  balance,
+  spend,
+  reset,
+  init: onInit,
+};
